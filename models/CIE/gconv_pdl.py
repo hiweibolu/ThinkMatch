304af70b196ad37971e0c31b8f424e59b98aa171
hiweibolu <53548456+hiweibolu@users.noreply.github.com>
2022-01-09 16:50:20 +0800

    Update gconv_pdl.py

--- models/CIE/gconv_pdl.py
+++ models/CIE/gconv_pdl.py
@@ -56,6 +56,104 @@ class Gconv(nn.Layer):
         x = paddle.bmm(A, F.relu(ax)) + F.relu(ux) # has size (bs, N, num_outputs)
         return x
 
+class ChannelIndependentConv(nn.Layer):
+    r"""
+    Channel Independent Embedding Convolution.
+    Proposed by `"Yu et al. Learning deep graph matching with channel-independent embedding and Hungarian attention.
+    ICLR 2020." <https://openreview.net/forum?id=rJgBd2NYPH>`_
+
+    :param in_features: the dimension of input node features
+    :param out_features: the dimension of output node features
+    :param in_edges: the dimension of input edge features
+    :param out_edges: (optional) the dimension of output edge features. It needs to be the same as ``out_features``
+    """
+    def __init__(self, in_features: int, out_features: int, in_edges: int, out_edges: int=None):
+        super(ChannelIndependentConv, self).__init__()
+        if out_edges is None:
+            out_edges = out_features
+        self.in_features = in_features
+        self.out_features = out_features
+        self.out_edges = out_edges
+        # self.node_fc = nn.Linear(in_features, out_features // self.out_edges)
+        # 参考给出的 Gconv.__init__ ，pytorch一行代码需要换成 paddle 若干行 :(
+        '''
+        self.node_fc = nn.Linear(in_features, out_features)
+        self.node_sfc = nn.Linear(in_features, out_features)
+        self.edge_fc = nn.Linear(in_edges, self.out_edges)
+        '''
+        k = math.sqrt(1.0 / in_features)
+        weight_attr_1 = paddle.ParamAttr(
+                initializer=paddle.nn.initializer.Uniform(-k, k))
+        bias_attr_1 = paddle.ParamAttr(
+                initializer=paddle.nn.initializer.Uniform(-k, k))
+        weight_attr_2 = paddle.ParamAttr(
+                initializer=paddle.nn.initializer.Uniform(-k, k))
+        bias_attr_2 = paddle.ParamAttr(
+                initializer=paddle.nn.initializer.Uniform(-k, k))
+        self.node_fc = nn.Linear(in_features, out_features,
+                                weight_attr=weight_attr_1, 
+                                bias_attr=bias_attr_1)
+        self.node_sfc = nn.Linear(in_features, out_features,
+                                weight_attr=weight_attr_2, 
+                                bias_attr=bias_attr_2)
+
+        
+        k = math.sqrt(1.0 / in_edges)
+        weight_attr_3 = paddle.ParamAttr(
+                initializer=paddle.nn.initializer.Uniform(-k, k))
+        bias_attr_3 = paddle.ParamAttr(
+                initializer=paddle.nn.initializer.Uniform(-k, k))
+        self.edge_fc = nn.Linear(in_edges, out_edges,
+                                weight_attr=weight_attr_3, 
+                                bias_attr=bias_attr_3)
+
+
+    #def forward(self, A: Tensor, emb_node: Tensor, emb_edge: Tensor, mode: int=1) -> Tuple[Tensor, Tensor]:
+    def forward(self, A, emb_node, emb_edge, mode: int=1):
+        r"""
+        :param A: :math:`(b\times n\times n)` {0,1} adjacency matrix. :math:`b`: batch size, :math:`n`: number of nodes
+        :param emb_node: :math:`(b\times n\times d_n)` input node embedding. :math:`d_n`: node feature dimension
+        :param emb_edge: :math:`(b\times n\times n\times d_e)` input edge embedding. :math:`d_e`: edge feature dimension
+        :param mode: 1 or 2, refer to the paper for details
+        :return: :math:`(b\times n\times d^\prime)` new node embedding,
+         :math:`(b\times n\times n\times d^\prime)` new edge embedding
+        """ # 简单地将 torch 换成 paddle
+        if mode == 1:
+            node_x = self.node_fc(emb_node)
+            node_sx = self.node_sfc(emb_node)
+            edge_x = self.edge_fc(emb_edge)
+
+            A = A.unsqueeze(-1)
+            A = paddle.multiply(A.expand_as(edge_x), edge_x)
+
+            node_x = paddle.matmul(A.transpose((0, 3, 1, 2)), node_x.unsqueeze(2).transpose((0, 3, 1, 2)))
+            node_x = node_x.squeeze(-1).transpose((0, 2, 1))
+            node_x = F.relu(node_x) + F.relu(node_sx)
+            edge_x = F.relu(edge_x)
+
+            return node_x, edge_x
+
+        elif mode == 2:
+            node_x = self.node_fc(emb_node)
+            node_sx = self.node_sfc(emb_node)
+            edge_x = self.edge_fc(emb_edge)
+
+            d_x = node_x.unsqueeze(1) - node_x.unsqueeze(2)
+            d_x = paddle.sum(d_x ** 2, dim=3, keepdim=False)
+            d_x = paddle.exp(-d_x)
+
+            A = A.unsqueeze(-1)
+            A = paddle.multiply(A.expand_as(edge_x), edge_x)
+
+            node_x = paddle.matmul(A.transpose((0, 3, 1, 2)), node_x.unsqueeze(2).transpose((0, 3, 1, 2)))
+            node_x = node_x.squeeze(-1).transpose((0, 2, 1))
+            node_x = F.relu(node_x) + F.relu(node_sx)
+            edge_x = F.relu(edge_x)
+            return node_x, edge_x
+
+        else:
+            raise ValueError('Unknown mode {}. Possible options: 1 or 2'.format(mode))
+
 class Siamese_ChannelIndependentConv(nn.Layer):
     r"""
     Siamese Channel Independent Conv neural network for processing arbitrary number of graphs.
